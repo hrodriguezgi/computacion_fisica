1189d7748ee55ac0454626fb31ed0afc7b59e5ac
Harvey Rodriguez Gil <harvey.rodriguez@finkargo.com>
2024-07-29 22:36:07 -0500

    Added class_7

---
+++ clase_7/mi_script.sh
@@ -0,0 +1,6 @@
+# Completar este archivo para que realice las siguientes actividades:
+# 1. Descargar el fichero laboratorio_7.tgz
+# 2. Imprimir el contenido del archivo1.txt
+# 3. Buscar la palabra 'registro' en archivo2.txt
+# 4. Crear una variable local llamada 'LOCAL_VAR' con el valor de 'amarillo azul rojo'
+# 5. Agregar el valor 'verde' en la posición 3, e imprimir la variable con todos los valores.
