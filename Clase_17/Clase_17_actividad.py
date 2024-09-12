653faf28a54a1d9c24c99965b5f63d81682f5a4b
Harvey Rodriguez Gil <harvey.rodriguez@finkargo.com>
2024-09-12 07:34:43 -0500

    Activity class 17

---
+++ Clase_17/Clase_17_actividad.py
@@ -0,0 +1,30 @@
+# Actividad
+
+"""
+Crear un programa en Python que calcule el BMI (Body Mass Index o Índice de
+Masa Corporal) de tres personas. El BMI se calcula utilizando la siguiente
+fórmula:
+
+BMI = peso(kg) / altura(m)^2
+
+Debes crear un diccionario donde cada clave sea el nombre de una persona y el
+valor asociado sea otro diccionario que contenga su peso en kilogramos y su
+altura en metros.
+
+Se deberá trabajar con los siguientes datos:
+- Juan: Peso: 70 kg, Altura: 1.75 m
+- Ana: Peso: 60 kg, Altura: 1.68 m
+- Carlos: Peso: 80 kg, Altura: 1.82 m
+"""
+
+# Inserte su código aquí
+
+
+# Resultado esperado:
+"""
+{
+    'Juan': {'peso': 70, 'altura': 1.75, 'BMI': 22.86},
+    'Ana': {'peso': 60, 'altura': 1.68, 'BMI': 21.26},
+    'Carlos': {'peso': 80, 'altura': 1.82, 'BMI': 24.15}
+}
+"""
